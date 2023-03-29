81423bc585c8b49019cc94ea6a556be5ca223774
Zmeegolov <karantinz@mail.ru>
2023-03-29 19:37:16 +0300

    Добавьте файлы проекта.

---
+++ junior4/Program.cs
@@ -0,0 +1,8 @@
+﻿int totalPictures = 52;
+int picturesRow = 3;
+
+int fullRows = totalPictures / picturesRow;
+int pictures = totalPictures % picturesRow;
+
+Console.WriteLine($"Количество заполненных рядов: {fullRows}");
+Console.WriteLine($"Количество картинок сверх меры: {pictures}");
